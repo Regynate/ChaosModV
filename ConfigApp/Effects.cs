1aee7fabdf17b330a182145719414ba8e2f13fd0
Veloscocity <134974775+Veloscocity@users.noreply.github.com>
2023-09-13 13:31:09 -0400

    New Effect: Boost Braking

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -429,6 +429,7 @@ namespace ConfigApp
             { "screen_bouncyradar", new EffectInfo("Bouncy Radar", EffectCategory.Screen, true) },
             { "misc_solid_props", new EffectInfo("Solid Props", EffectCategory.Misc, true) },
             { "misc_grounded", new EffectInfo("Grounded", EffectCategory.Misc, true) },
+            { "veh_boostbrake", new EffectInfo("Boost Braking", EffectCategory.Vehicle, true) },
         };
     }
 }
