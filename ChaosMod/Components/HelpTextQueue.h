55a614583f284be80e61987fba3b68dea9b274f9
Regynate <64607261+Regynate@users.noreply.github.com>
2025-03-24 19:12:27 +0300

    Fixed timed helptexts

--- ChaosMod/Components/HelpTextQueue.h
+++ ChaosMod/Components/HelpTextQueue.h
@@ -8,13 +8,13 @@ class HelpTextQueue : public Component
 {
 	struct HelpText
 	{
-		std::string_view Text;
+		std::string Text;
 		float TimerSecs;
 	};
 	std::queue<HelpText> m_HelpTextQueue;
 
   public:
-	void DisplayLabel(std::string_view label, std::uint8_t durationSecs);
+	void DisplayLabel(std::string label, std::uint8_t durationSecs);
 
 	virtual void OnModPauseCleanup() override;
 	virtual void OnRun() override;
