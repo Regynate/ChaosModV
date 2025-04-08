c6564155e04fe6fa838decef48803b31bb001e11
Regynate <64607261+Regynate@users.noreply.github.com>
2025-04-08 11:00:03 +0300

    Jump And Boost: changed to actual controls; fine tuned boost

--- ChaosMod/Effects/db/Vehs/VehsJumpAndBoost.cpp
+++ ChaosMod/Effects/db/Vehs/VehsJumpAndBoost.cpp
@@ -5,40 +5,59 @@
 
 static void OnTick()
 {
-	DisplayHelpText("Jump = SPACE | Boost = X");
+	static bool hasJumped  = false;
+	static bool hasBoosted = false;
 
-	auto const player    = PLAYER_PED_ID();
-	auto const inVehicle = IS_PED_IN_ANY_VEHICLE(player, false);
+	auto const player      = PLAYER_PED_ID();
+	auto const inVehicle   = IS_PED_IN_ANY_VEHICLE(player, false);
 	if (!inVehicle)
+	{
+		hasBoosted = hasJumped = false;
 		return;
+	}
 
 	auto const vehicle        = GET_VEHICLE_PED_IS_IN(player, false);
 	auto const isVehicleInAir = IS_ENTITY_IN_AIR(vehicle);
-
-	if (GetAsyncKeyState(VK_SPACE) && !isVehicleInAir)
-		APPLY_FORCE_TO_ENTITY(vehicle, 1, 0.0f, 0.0f, 25.0f, 0.0f, 0.0f, 0.0f, 0, true, true, true, false, false);
-	auto constexpr X_KEY = 0x58;
-	if (GetAsyncKeyState(X_KEY))
+	
+	if (!isVehicleInAir)
 	{
-		for (auto const _ : std::ranges::iota_view { 0, 150 })
+		hasJumped  = false;
+		hasBoosted = false;
+		DisplayHelpText("Jump = ~INPUT_VEH_JUMP~");
+
+		if (IS_CONTROL_JUST_PRESSED(0, 102))
 		{
-			ANIMPOSTFX_PLAY("RaceTurbo", 10000, true);
-
-			auto const currentSpeed = GET_ENTITY_SPEED(vehicle);
-			SET_VEHICLE_FORWARD_SPEED(vehicle, currentSpeed + 1);
-			WAIT(0);
+			APPLY_FORCE_TO_ENTITY(vehicle, 1, 0.0f, 0.0f, 60.0f, 0.0f, 0.0f, 0.0f, 0, true, true, true, false, false);
+			hasJumped = true;
 		}
+	}
 
-		ANIMPOSTFX_STOP("RaceTurbo");
+	if (hasJumped && !hasBoosted)
+	{
+		DisplayHelpText("Boost = ~INPUT_VEH_DROP_PROJECTILE~");
 
-		while (IS_ENTITY_IN_AIR(vehicle))
-			WAIT(100);
+		if (IS_CONTROL_JUST_PRESSED(0, 105) && hasJumped && !hasBoosted)
+		{
+			auto startTick = GET_GAME_TIMER();
+
+			while (GET_GAME_TIMER() - startTick < 1000)
+			{
+				ANIMPOSTFX_PLAY("RaceTurbo", 10000, true);
+
+				auto const currentSpeed = GET_ENTITY_SPEED(vehicle);
+				SET_VEHICLE_FORWARD_SPEED(vehicle, currentSpeed + GET_FRAME_TIME() * 50);
+				WAIT(0);
+			}
+
+			ANIMPOSTFX_STOP("RaceTurbo");
+			hasBoosted = true;
+		}
 	}
 }
 
 // clang-format off
-REGISTER_EFFECT(nullptr, nullptr, OnTick, 
-    {
+	REGISTER_EFFECT(nullptr, nullptr, OnTick, 
+		{
         .Name = "Jump And Boost", 
         .Id = "vehs_jump_and_boost", 
         .IsTimed = true
