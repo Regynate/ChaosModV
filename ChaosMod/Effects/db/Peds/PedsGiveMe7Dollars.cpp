bcbe7cbaaa516456c2b69980672952ec65bc8109
Regynate <64607261+Regynate@users.noreply.github.com>
2025-04-08 10:42:09 +0300

    Give Me 7 Dollars: don't make Denise act scared; changed driving style

--- ChaosMod/Effects/db/Peds/PedsGiveMe7Dollars.cpp
+++ ChaosMod/Effects/db/Peds/PedsGiveMe7Dollars.cpp
@@ -43,9 +43,9 @@ static void SetPlayerMoney(const int amount)
 }
 
 CHAOS_VAR auto constexpr PED_THROWN_OUT_FLAG = 4160;
-CHAOS_VAR auto constexpr MUG_AMOUNT       = 7;
+CHAOS_VAR auto constexpr MUG_AMOUNT          = 7;
 CHAOS_VAR auto constexpr PED_TYPE_CIVFEMALE  = 6;
-CHAOS_VAR auto constexpr DENISE_MODEL                      = "cs_denise";
+CHAOS_VAR auto constexpr DENISE_MODEL        = "ig_denise";
 
 static void RemoveSevenDollars()
 {
@@ -53,40 +53,29 @@ static void RemoveSevenDollars()
 	SetPlayerMoney(playerMoney - MUG_AMOUNT);
 }
 
-static Ped SpawnDenise(Vector3 coords)
-{
-	auto const deniseModelHash = GET_HASH_KEY(DENISE_MODEL);
-	LoadModel(deniseModelHash);
-
-	return CREATE_PED(PED_TYPE_CIVFEMALE, deniseModelHash, coords.x + 1.0f, coords.y, coords.z, 0.0f, true, true);
-}
-
 static void TriggerDeniseMugging()
 {
-	auto const playerPed     = PLAYER_PED_ID();
-	auto const playerCoords  = GET_ENTITY_COORDS(playerPed, true);
-	auto const denisePed     = SpawnDenise(playerCoords);
-	auto const playerVehicle = GET_VEHICLE_PED_IS_IN(playerPed, false);
+	auto const playerPed    = PLAYER_PED_ID();
+	auto const playerCoords = GET_ENTITY_COORDS(playerPed, true);
+	auto const denisePed    = CreatePoolPed(PED_TYPE_CIVFEMALE, GET_HASH_KEY(DENISE_MODEL), playerCoords.x, playerCoords.y, playerCoords.z, 0.0f);
 
+	SET_PED_MONEY(denisePed, 777);
 	SET_PED_CAN_RAGDOLL(denisePed, false);
 	RemoveSevenDollars();
 
 	if (!IS_PED_IN_ANY_VEHICLE(playerPed, false))
 	{
-		TASK_REACT_AND_FLEE_PED(denisePed, playerPed);
-		return;
+		TASK_SMART_FLEE_PED(denisePed, playerPed, 10000.f, -1, false, false);
 	}
+	else
+	{
+		auto const playerVehicle = GET_VEHICLE_PED_IS_IN(playerPed, false);
 
-	TASK_LEAVE_VEHICLE(playerPed, playerVehicle, PED_THROWN_OUT_FLAG);
-	WAIT(1000);
+		BRING_VEHICLE_TO_HALT(playerVehicle, 0.1f, 10, 0);
 
-	SET_PED_INTO_VEHICLE(denisePed, playerVehicle, -1);
-	WAIT(1000);
-
-	float fleeX = playerCoords.x + ((GET_RANDOM_INT_IN_RANGE(0, 2) == 0) ? -200.0f : 200.0f);
-	TASK_VEHICLE_DRIVE_TO_COORD_LONGRANGE(denisePed, playerVehicle, fleeX, playerCoords.y, playerCoords.z, 100.0f,
-	                                      786603, 5.0f);
-	SET_PED_KEEP_TASK(denisePed, true);
+		TASK_VEHICLE_DRIVE_WANDER(denisePed, playerVehicle, 100.0f, 572);
+		SET_PED_KEEP_TASK(denisePed, true);
+	}
 }
 
 static void OnStart()
